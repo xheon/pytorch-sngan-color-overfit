e14270a3d6c7beaf8c60633d69b81d2504899701
Manuel Dahnert <manuel.dahnert@gmail.com>
2020-05-04 15:50:46 +0200

    Add bias in SN discriminator.

--- discriminator.py
+++ discriminator.py
@@ -7,27 +7,27 @@ class Discriminator(nn.Module):
         super().__init__()
 
         self.model = nn.Sequential(
-            torch.nn.utils.spectral_norm(nn.Conv2d(3, 16, 3, 2, 1, bias=False)), # 32
+            torch.nn.utils.spectral_norm(nn.Conv2d(3, 16, 3, 2, 1)), # 32
             nn.LeakyReLU(0.2, inplace=True),
             # nn.BatchNorm2d(16),
 
-            torch.nn.utils.spectral_norm(nn.Conv2d(16, 32, 3, 2, 1, bias=False)), # 16
+            torch.nn.utils.spectral_norm(nn.Conv2d(16, 32, 3, 2, 1)), # 16
             nn.LeakyReLU(0.2, inplace=True),
             # nn.BatchNorm2d(32),
 
-            torch.nn.utils.spectral_norm(nn.Conv2d(32, 64, 3, 2, 1, bias=False)), # 8
+            torch.nn.utils.spectral_norm(nn.Conv2d(32, 64, 3, 2, 1)), # 8
             nn.LeakyReLU(0.2, inplace=True),
             # nn.BatchNorm2d(64),
 
-            torch.nn.utils.spectral_norm(nn.Conv2d(64, 128, 3, 2, 1, bias=False)), # 4
+            torch.nn.utils.spectral_norm(nn.Conv2d(64, 128, 3, 2, 1)), # 4
             nn.LeakyReLU(0.2, inplace=True),
             # nn.BatchNorm2d(128),
 
-            torch.nn.utils.spectral_norm(nn.Conv2d(128, 256, 3, 2, 1, bias=False)),  # 2
+            torch.nn.utils.spectral_norm(nn.Conv2d(128, 256, 3, 2, 1)),  # 2
             nn.LeakyReLU(0.2, inplace=True),
             # nn.BatchNorm2d(256),
 
-            torch.nn.utils.spectral_norm(nn.Conv2d(256, 512, 2, 1, 0, bias=False)),  # 1
+            torch.nn.utils.spectral_norm(nn.Conv2d(256, 512, 2, 1, 0)),  # 1
             nn.LeakyReLU(0.2, inplace=True),
             # nn.BatchNorm2d(512),
 
